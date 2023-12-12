ce9db996200cc37577da3e684bcb551e6e33e6e1
Vishnu Kumar <vermaji1891@gmail.com>
2023-12-12 14:09:16 +0530

    added productDetails page

--- src/Components/ProductBox/ProductBox.jsx
+++ src/Components/ProductBox/ProductBox.jsx
@@ -1,6 +1,8 @@
+import { Link } from "react-router-dom";
+
 function ProductBox ({price, name}) {
     return (
-            <>
+            <Link to={"/products/2"}>
                 <div class="relative aspect-[16/9]  w-auto rounded-md md:aspect-auto md:h-[400px]">
                     <img
                         src="https://images.unsplash.com/photo-1588099768531-a72d4a198538?ixlib=rb-4.0.3&amp;ixid=MnwxMjA3fDB8MHxjb2xsZWN0aW9uLXBhZ2V8NnwxMTM4MTU1NXx8ZW58MHx8fHw%3D&amp;auto=format&amp;fit=crop&amp;w=800&amp;q=60"
@@ -17,7 +19,7 @@ function ProductBox ({price, name}) {
                         </button>
                     </div>
                 </div>
-            </>
+            </Link>
             
 
     )
--- src/Routes/MainRoutes.jsx
+++ src/Routes/MainRoutes.jsx
@@ -2,12 +2,14 @@ import { Route, Routes } from "react-router-dom";
 import Home from "../Pages/Home";
 import Error from "../Pages/Error";
 import ProductList from "../Pages/ProductList/ProductList";
+import ProductDetails from "../Pages/ProductDetails/ProductDetails";
 
 function MainRoutes () {
     return (
         <Routes>
             <Route  path="/" element={<Home />}/>
             <Route  path="/products" element={<ProductList />}/>
+            <Route  path="/products/:id" element={<ProductDetails />}/>
             <Route  path="*" element={<Error />}/>
         </Routes>
     )
