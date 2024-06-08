2d2b98041d30a28fb25d0c801cc84cbc232df7d9
Vishnu Kumar <vermaji1891@gmail.com>
2024-06-08 15:14:51 +0530

    added mobile and replace nav bar with responsive nav bar

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,114 +1,149 @@
 import { jwtDecode } from "jwt-decode";
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { useEffect } from "react";
 import { useCookies } from "react-cookie";
 import { Link } from "react-router-dom";
 import UserContext from "../../context/UserContext";
 import CartContext from "../../context/CartContext";
 import axiosinstance from "../../Config/AxiosInstance";
+import Logo from "../../assets/logo.png";
 
-function Header () {
-    
-    const [token, setToken, removeToken] = useCookies(["jwt-token"])
-    const {user, setUser} = useContext(UserContext)
-    const {cart, setCart} = useContext(CartContext)
-    
-    function logOut() {
-        setUser(null)
-        setCart(null)
-        removeToken("jwt-token", {httpOnly: true})
-        axiosinstance.get("logout", {withCredentials: true})
-    }
+function Header() {
+  const [token, setToken, removeToken] = useCookies(["jwt-token"]);
+  const { user, setUser } = useContext(UserContext);
+  const { cart, setCart } = useContext(CartContext);
+  const [showNav, setShowNav] = useState(true)
 
-    useEffect(() => {
 
-    }, [token])
-    return (
-        <div className="relative w-full bg-gray-300">
-            <div className="mx-auto flex max-w-7xl items-center justify-between px-4 py-2 sm:px-6 lg:px-8">
-                <div className="inline-flex items-center space-x-2">
-                
-                <Link to={"/"}>
-                 <span className="font-semibold text-2xl mx-10">ShopCart</span>
+  function logOut() {
+    setUser(null);
+    setCart(null);
+    removeToken("jwt-token", { httpOnly: true });
+    axiosinstance.get("logout", { withCredentials: true });
+  }
 
-                </Link>
-                <div className="flex grow justify-end">
-                    <input
-                        className="flex h-10 w-[250px] rounded-md bg-gray-100 px-3 py-2 text-sm placeholder:text-gray-600 focus:outline-none focus:ring-1 focus:ring-black/30 focus:ring-offset-1 disabled:cursor-not-allowed disabled:opacity-50"
-                        type="text"
-                        placeholder="Serach"
-                    />
-                </div>
-                </div>
-                <div className="hidden grow items-start lg:flex">
-                
-                
-                
-                </div>
-                <div className="hidden space-x-2 lg:block">
-                <ul className="ml-12 inline-flex space-x-8">
-                  
-                {user && <li>
-                    <a
-                        href="#"
-                        className="inline-flex items-center text-lg  font-semibold  hover:text-gray-700"
-                    >
-                        {user.username}
-                        
-                    </a>
-                    </li>
-                        
-                    }
-                    {user && cart && <li>
-                    <Link to={`cart/${user.id}`}
-                        // href="#"
-                        className="inline-flex items-center text-lg  font-semibold hover:text-gray-700"
-                    >
-                        Cart{"  " + cart && cart.products && ` (${cart.products.length})`}
-                        
-                    </Link>
-                    </li>}
-                </ul>
-            {token["jwt-token"] ?<Link
+  useEffect(() => {}, [token]);
+  return (
+    <>
+      
+
+      {/* navbar start here */}
+
+      <nav className="bg-[#373A40] z-10 p-2 flex justify-between sticky top-0">
+      {showNav ? (
+        <div className="flex justify-between w-full">
+          <div className="flex p-2 items-center gap-10">
+            <div>
+              <Link to={"/"}  className="flex gap-2 items-center">
+                <img className="object-cover max-w-12 max-h-12" src={Logo} alt="Logo" />
+                <span className="text-2xl font-medium text-white">ShopKaro</span>
+              </Link>
+            </div>
+          </div>
+          <div className="relative hidden md:flex items-center w-1/4">
+            <div className="hidden absolute inset-y-0 left-0 pl-3 lg:flex items-center pointer-events-none">
+              <i className="fa-solid fa-magnifying-glass text-gray-400"></i>
+            </div>
+            <input
+              type="text"
+              placeholder="Search Your item here..."
+              className="p-2 border border-gray-300 w-full text-center rounded-lg focus:outline-none focus:ring-2 focus:ring-gray-400 focus:border-transparent"
+            />
+          </div>
+          <div className="hidden md:flex p-2 font-medium items-center gap-8">
+            <p className="text-xl hidden md:block text-white">{(user?.username) ? user?.username : "UserName"}</p>
+            
+            <div className="relative">
+              <i className="fa-solid fa-cart-shopping text-2xl text-white"></i>
+              <span className="absolute top-[-8px] right-[-5px] bg-red-600 text-white rounded-full text-xs w-4 h-4 flex items-center justify-center">{cart?.products?.length}</span>
+            </div>
+            <Link to={`cart/${user?.id}`} className="text-lg text-white cursor-pointer">Cart</Link>
+
+            {token["jwt-token"] ? (
+              <Link
                 type="button"
                 onClick={() => {
-                   logOut()
+                  logOut();
                 }}
-                    
-                className="rounded-md bg-green-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-green-600/80 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-green-600"
-                >
+                className="rounded-md bg-black px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-black/80 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-green-600"
+              >
                 Log Out
-            </Link>  : 
-            <Link  to={"/signin"}
+              </Link>
+            ) : (
+              <Link
+                to={"/signin"}
                 type="button"
-                className="rounded-md bg-black px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-black/80 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-black"
-            >
+                className="rounded-md bg-green-500 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-green-600/80 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-black"
+              >
                 Log In
-            </Link>
-            }
+              </Link>
+            )}
             
-                </div>
-                <div className="lg:hidden">
-                <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    width="24"
-                    height="24"
-                    viewBox="0 0 24 24"
-                    fill="none"
-                    stroke="currentColor"
-                    strokeWidth="2"
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    className="h-6 w-6 cursor-pointer"
-                >
-                    <line x1="4" y1="12" x2="20" y2="12"></line>
-                    <line x1="4" y1="6" x2="20" y2="6"></line>
-                    <line x1="4" y1="18" x2="20" y2="18"></line>
-                </svg>
-                </div>
-            </div>
+
+          </div>
+          <button className="p-2 md:hidden" onClick={() => setShowNav(!showNav)}>
+            <i className="fa-solid fa-bars text-gray-400 text-2xl"></i>
+          </button>
         </div>
-    );
+      ) : (
+        <div id="nav-dialog" className="bg-white fixed z-10 md:hidden inset-0 p-3">
+          <div id="nav-bar" className="flex justify-between">
+            <a href="#" id="brand" className="flex gap-2 items-center">
+              <img className="object-cover max-w-12 max-h-12" src={Logo} alt="Logo" />
+              <span className="text-2xl font-medium">ShopKaro</span>
+            </a>
+            <button className="p-2 lg:hidden"  onClick={() => setShowNav(!showNav)}>
+              <i className="fa-solid text-2xl fa-xmark text-gray-600"></i>
+            </button>
+          </div>
+          <div className="mt-6">
+            <div className="relative flex items-center w-full mb-4">
+              <div className="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
+                <i className="fa-solid fa-magnifying-glass text-gray-400"></i>
+              </div>
+              <input
+                type="text"
+                placeholder="Search Your item here..."
+                className="p-2 border border-gray-300 w-full text-center rounded-lg focus:outline-none focus:ring-2 focus:ring-gray-400 focus:border-transparent"
+              />
+            </div>
+            <div className="h-[1px] bg-gray-300"></div>
+            <div className="md:flex p-2 font-medium flex flex-col items-center gap-8">
+              <p className="text-xl md:block">UserName</p>
+              <div className="relative">
+                <i className="fa-solid fa-cart-shopping text-2xl"></i>
+                <span className="absolute top-[-8px] right-[-5px] bg-red-600 text-white rounded-full text-xs w-4 h-4 flex items-center justify-center">{cart?.products?.length}</span>
+              </div>
+              <Link to={`cart/${user?.id}`} className="text-xl cursor-pointer">Cart</Link>
+              {token["jwt-token"] ? (
+              <Link
+                type="button"
+                onClick={() => {
+                  logOut();
+                }}
+                className="rounded-md bg-black px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-black/80 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-green-600"
+              >
+                Log Out
+              </Link>
+            ) : (
+              <Link
+                to={"/signin"}
+                type="button"
+                className="rounded-md bg-green-500 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-green-600/80 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-black"
+              >
+                Log In
+              </Link>
+            )}
+
+              
+            </div>
+          </div>
+          <div className="h-[1px] bg-gray-300"></div>
+        </div>
+      )}
+    </nav>
+    </>
+  );
 }
 
 export default Header;
